792531dc3f851465f92227f353601aafbc8b97bb
KISHORERAJA28 <kishoreraaja@gmail.com>
2026-04-09 22:30:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,6 @@ from helpers import apology, login_required, lookup, usd
 # Configure application
 app = Flask(__name__)
 
-# Custom filter
-app.jinja_env.filters["usd"] = usd
-
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
-
 
 @app.after_request
 def after_request(response):
